0d24175731981c2628d829532cf6d434e2f7afd7
Pierre Cote <Pierre@LogicielCote.COM>
2025-09-13 18:48:41 -0700

    String formatting

--- Tutorial/HelloWorld.py
+++ Tutorial/HelloWorld.py
@@ -1,5 +1,11 @@
+name = "Pierre Cote"
+
+line1 = f"Hello, {name}"
 print("Hello, World!")
-print("Hello, Pierre!")
-print("Hello, World!")
+print(line1)
+
+count = len(name)
+line2 = f"Your name has '{count:02X}' characters"
+print(line2)
 
 print("Done")
