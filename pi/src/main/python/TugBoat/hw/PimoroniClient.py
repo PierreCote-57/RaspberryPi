37bf4c68dc56df8fcc0ed97871995c756e0730c4
Pierre Cote <92542127+PierreCote-57@users.noreply.github.com>
2025-09-29 07:10:13 -0700

    Calibrate LED RGB

--- pi/src/main/python/TugBoat/hw/PimoroniClient.py
+++ pi/src/main/python/TugBoat/hw/PimoroniClient.py
@@ -55,12 +55,12 @@ class ServoClient:
     def setRGB(self, n, r, g, b):
         command = "$Light,SetRGB,{0},{1},{2},{3}".format(n, r, g, b)
         answer = self.processCommand(command)
-        return answer
+        return AnswerRGB(answer)
     
     def setHSV(self, n, h, s, b):
         command = "$Light,SetHSV,{0},{1},{2},{3}".format(n, h, s, v)
         answer = self.processCommand(command)
-        return answer
+        return AnswerRGB(answer)
     
     def readVoltage(self):
         return self.readSensor("Volt")
@@ -80,17 +80,43 @@ class ServoClient:
         return answer
 
 
+class AnswerRGB:
+    def __init__(self, answer):
+        self.answer = answer;
+        self.result = answer[:2]
+        rgbParts= answer[4:len(answer)-3].split(",")
+        self.r = int(rgbParts[0].replace("'", ""))
+        self.g = int(rgbParts[1].replace("'", ""))
+        self.b = int(rgbParts[2].replace("'", ""))
+        self.s = int(rgbParts[3].replace("'", ""))
+
+    def __str__(self):
+        return f"[{self.r:3}, {self.g:3}, {self.b:3}, {self.s:3}]"
+
 
 def testRGB(servo):
-    pause = 2.0
+    pause = 1.0
     servo.clearLight()
     time.sleep(pause)
-    servo.setRGB(1, 0.5, 0.0, 0.0)
+    servo.setRGB(1, 64, 0, 0)
     time.sleep(pause)
-    servo.setRGB(2, 0.0, 0.5, 0.0)
+    servo.setRGB(2, 0, 64, 0)
     time.sleep(pause)
-    servo.setRGB(3, 0.0, 0.0,0.5)
+    servo.setRGB(3, 0, 0, 64)
     time.sleep(pause)
+    servo.clearLight()
+
+def calibrateRGB(servo):
+    values = range(0, 256, 8)
+    pauseSec = 0.1
+    for x in values:
+        answerR = servo.setRGB(1, x, 0, 0)
+        time.sleep(pauseSec)
+        answerG = servo.setRGB(1, 0, x, 0)
+        time.sleep(pauseSec)
+        answerB = servo.setRGB(1, 0, 0, x)
+        time.sleep(pauseSec)
+        print(f"At {x:3}: R={answerR}; G={answerG}; B={answerB}")
     servo.clearLight()
 
 
@@ -112,5 +138,6 @@ if __name__ == "__main__":
     timer.checkpoint("Writing ")
 
     servo = ServoClient()
-#    testRGB(servo)
+    testRGB(servo)
+    calibrateRGB(servo)
     testSensor(servo)
